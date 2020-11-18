ad0935aada1d1f29fc1d9da489e1fd3476578ead
Victor de Oliveira <oliveira.wn@gmail.com>
2020-11-18 18:45:27 -0300

    firebase config

---
+++ components/firebaseConfig.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: 'AIzaSyDH6C-h7sSGexlk9tyA6FyUK7kNM055uJ4',
+    authDomain: 'crud-employees-b4ce8.firebaseapp.com',
+    databaseURL: 'https://crud-employees-b4ce8.firebaseio.com',
+    projectId: 'crud-employees-b4ce8',
+    storageBucket: 'crud-employees-b4ce8.appspot.com',
+    messagingSenderId: '273298623839',
+    appId: '1:273298623839:web:3b8431d8e17188416bbaea'
+}
+export default firebase.initializeApp(firebaseConfig)
